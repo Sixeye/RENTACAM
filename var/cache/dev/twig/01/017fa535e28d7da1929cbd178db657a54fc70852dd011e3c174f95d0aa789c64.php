9d7c732e453e1453ba962d5bc55028bd50b4e0da
Sixeye <srinath@srinath.fr>
2019-05-27 16:33:03 +0200

    Pages added

---
+++ var/cache/dev/twig/01/017fa535e28d7da1929cbd178db657a54fc70852dd011e3c174f95d0aa789c64.php
@@ -0,0 +1,211 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* base.html.twig */
+class __TwigTemplate_8b62dde7f22e94a846899ef039eae35b087fc801d71e37cea78beeadb1d78475 extends \Twig\Template
+{
+    private $source;
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+            'title' => [$this, 'block_title'],
+            'stylesheets' => [$this, 'block_stylesheets'],
+            'body' => [$this, 'block_body'],
+            'javascripts' => [$this, 'block_javascripts'],
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "base.html.twig"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "base.html.twig"));
+
+        // line 1
+        echo "<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=\"UTF-8\">
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, shrink-to-fit=no\">
+
+        <title>";
+        // line 7
+        $this->displayBlock('title', $context, $blocks);
+        echo "</title>
+        ";
+        // line 8
+        $this->displayBlock('stylesheets', $context, $blocks);
+        // line 18
+        echo "    </head>
+    <body>
+        ";
+        // line 20
+        $this->displayBlock('body', $context, $blocks);
+        // line 23
+        echo "        ";
+        $this->displayBlock('javascripts', $context, $blocks);
+        // line 29
+        echo "    </body>
+</html>
+";
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    // line 7
+    public function block_title($context, array $blocks = [])
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        echo "RENTACAM";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 8
+    public function block_stylesheets($context, array $blocks = [])
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "stylesheets"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "stylesheets"));
+
+        echo "  <link rel=\"stylesheet\" href=\"/bootstrap/css/bootstrap.min.css\">
+            <link rel=\"stylesheet\" href=\"/fonts/font-awesome.min.css\">
+            <link rel=\"stylesheet\" href=\"/fonts/ionicons.min.css\">
+            <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700\">
+            <link rel=\"stylesheet\" href=\"/css/Features-Boxed.css\">
+            <link rel=\"stylesheet\" href=\"/css/Footer-Dark.css\">
+            <link rel=\"stylesheet\" href=\"/css/Header-Blue--Sticky-Header--Smooth-Scroll.css\">
+            <link rel=\"stylesheet\" href=\"/css/styles.css\">
+            <link rel=\"stylesheet\" href=\"/css/Team-Boxed.css\">
+        ";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 20
+    public function block_body($context, array $blocks = [])
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        // line 21
+        echo "
+        ";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 23
+    public function block_javascripts($context, array $blocks = [])
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "javascripts"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "javascripts"));
+
+        // line 24
+        echo "            <script src=\"/js/jquery.min.js\"></script>
+            <script src=\"/bootstrap/js/bootstrap.min.js\"></script>
+            <script src=\"/js/Header-Blue--Sticky-Header--Smooth-Scroll-1.js\"></script>
+            <script src=\"/js/Header-Blue--Sticky-Header--Smooth-Scroll.js\"></script>
+        ";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "base.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  154 => 24,  145 => 23,  134 => 21,  125 => 20,  98 => 8,  80 => 7,  68 => 29,  65 => 23,  63 => 20,  59 => 18,  57 => 8,  53 => 7,  45 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=\"UTF-8\">
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, shrink-to-fit=no\">
+
+        <title>{% block title %}RENTACAM{% endblock %}</title>
+        {% block stylesheets %}  <link rel=\"stylesheet\" href=\"/bootstrap/css/bootstrap.min.css\">
+            <link rel=\"stylesheet\" href=\"/fonts/font-awesome.min.css\">
+            <link rel=\"stylesheet\" href=\"/fonts/ionicons.min.css\">
+            <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700\">
+            <link rel=\"stylesheet\" href=\"/css/Features-Boxed.css\">
+            <link rel=\"stylesheet\" href=\"/css/Footer-Dark.css\">
+            <link rel=\"stylesheet\" href=\"/css/Header-Blue--Sticky-Header--Smooth-Scroll.css\">
+            <link rel=\"stylesheet\" href=\"/css/styles.css\">
+            <link rel=\"stylesheet\" href=\"/css/Team-Boxed.css\">
+        {% endblock %}
+    </head>
+    <body>
+        {% block body %}
+
+        {% endblock %}
+        {% block javascripts %}
+            <script src=\"/js/jquery.min.js\"></script>
+            <script src=\"/bootstrap/js/bootstrap.min.js\"></script>
+            <script src=\"/js/Header-Blue--Sticky-Header--Smooth-Scroll-1.js\"></script>
+            <script src=\"/js/Header-Blue--Sticky-Header--Smooth-Scroll.js\"></script>
+        {% endblock %}
+    </body>
+</html>
+", "base.html.twig", "/Users/srinathchristophersamarasinghe/Documents/WEB DEVELOPMENT/RENTACAM/RENTACAM/templates/base.html.twig");
+    }
+}
---
+++ var/cache/dev/twig/6b/6b35cdfab124cbced9a67b6ca251b7acee764e649e1c807958000335f070e3d4.php
@@ -0,0 +1,246 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* index/index.html.twig */
+class __TwigTemplate_1e885223d0c6a7b6c9db21facaa6e835527d48ba8ce6a2e060f7614f79dc1961 extends \Twig\Template
+{
+    private $source;
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+            'title' => [$this, 'block_title'],
+            'body' => [$this, 'block_body'],
+        ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "index/index.html.twig"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "index/index.html.twig"));
+
+        $this->parent = $this->loadTemplate("base.html.twig", "index/index.html.twig", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    // line 3
+    public function block_title($context, array $blocks = [])
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        echo "Bienvenue!";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 5
+    public function block_body($context, array $blocks = [])
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        // line 6
+        echo "    <div id=\"home\">
+        <div class=\"header-blue\">
+            <nav class=\"navbar navbar-dark navbar-expand-md sticker\" style=\"padding-top:30px;\">
+                <div class=\"container\"><a class=\"navbar-brand\" style=\"font-size:24px;\" href=\"#\">RENTACAM</a><button data-toggle=\"collapse\" class=\"navbar-toggler\" data-target=\"#navcol-1\"><span class=\"sr-only\">Toggle navigation</span><span class=\"navbar-toggler-icon\"></span></button>
+                    <div
+                            class=\"collapse navbar-collapse\" id=\"navcol-1\">
+                        <ul class=\"nav navbar-nav ml-auto\">
+                            <li class=\"nav-item\" role=\"presentation\"><a class=\"nav-link active\" href=\"index.html\" target=\"_top\">Bienvenue</a></li>
+                            <li class=\"nav-item\" role=\"presentation\"><a class=\"nav-link active\" href=\"cameraalouer.html\">Caméras à louer</a></li>
+                            <li class=\"nav-item\" role=\"presentation\"><a class=\"nav-link active\" href=\"lesloueurs.html\">Les loueurs</a></li>
+                            <li class=\"nav-item\" role=\"presentation\"><a class=\"nav-link active\" href=\"mettreunecameralocation.html\">Mettre une caméra en location </a></li>
+                            <li class=\"nav-item\" role=\"presentation\"><a class=\"nav-link active\" href=\"modifieruneannonce.html\">Modifier une annonce</a></li>
+                            <li class=\"nav-item\" role=\"presentation\"></li>
+                            <li class=\"nav-item\" role=\"presentation\"></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div class=\"container hero\">
+                <div class=\"row\">
+                    <div class=\"col-12 col-lg-6 col-xl-6 offset-xl-1\" style=\"margin-left:44px;\">
+                        <h1 style=\"margin-top:204px;\">La révolution de vos tournages</h1>
+                        <p>Vous allez pouvoir louer la caméra de votre choix à un particulier avec entre vous un contrat d'assurance.</p><button class=\"btn btn-light btn-lg action-button\" style=\"padding-right:20px;padding-left:20px;padding-bottom:6px;padding-top:6px;font-size:16px;\"
+                                                                                                                                                 hover=\"background: #fff; color: #333;\" type=\"button\">En savoir plus</button></div>
+                    <div class=\"col-md-5 col-lg-5 offset-lg-1 offset-xl-0 d-none d-lg-block phone-holder\"><img src=\"/img/VIPER_THOMSON.png\">
+                        <div class=\"iphone-mockup\">
+                            <div class=\"screen\"><img style=\"width:261px;\" src=\"assets/img/VIPER_THOMSON.png\"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class=\"footer-dark\">
+        <footer>
+            <div class=\"container\">
+                <div class=\"row\">
+                    <div class=\"col-sm-6 col-md-3 item\">
+                        <h3>Services</h3>
+                        <ul>
+                            <li><a href=\"#\">Collaborer avec nous</a></li>
+                            <li><a href=\"#\">Cookies</a></li>
+                            <li><a href=\"#\">Mentions légales</a></li>
+                            <li><a href=\"#\">Se connecter</a></li>
+                        </ul>
+                    </div>
+                    <div class=\"col-sm-6 col-md-3 item\">
+                        <h3>A propos</h3>
+                        <ul>
+                            <li><a href=\"#\">La société</a></li>
+                            <li><a href=\"#\">La team</a></li>
+                            <li><a href=\"#\">Nous rejoindre</a></li>
+                        </ul>
+                    </div>
+                    <div class=\"col-md-6 item text\">
+                        <h3>RENTACAM</h3>
+                        <p>Service de mise en relation de particuliers pour des fins de location de matériel cinématographique avec assurance.</p>
+                    </div>
+                    <div class=\"col item social\"><a href=\"#\"><i class=\"icon ion-social-facebook\"></i></a><a href=\"#\"><i class=\"icon ion-social-twitter\"></i></a><a href=\"#\"><i class=\"icon ion-social-instagram\"></i></a></div>
+                </div>
+                <p class=\"copyright\">RENTACAM © 2019</p>
+            </div>
+        </footer>
+    </div>
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "index/index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  84 => 6,  75 => 5,  57 => 3,  35 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{% extends 'base.html.twig' %}
+
+{% block title %}Bienvenue!{% endblock %}
+
+{% block body %}
+    <div id=\"home\">
+        <div class=\"header-blue\">
+            <nav class=\"navbar navbar-dark navbar-expand-md sticker\" style=\"padding-top:30px;\">
+                <div class=\"container\"><a class=\"navbar-brand\" style=\"font-size:24px;\" href=\"#\">RENTACAM</a><button data-toggle=\"collapse\" class=\"navbar-toggler\" data-target=\"#navcol-1\"><span class=\"sr-only\">Toggle navigation</span><span class=\"navbar-toggler-icon\"></span></button>
+                    <div
+                            class=\"collapse navbar-collapse\" id=\"navcol-1\">
+                        <ul class=\"nav navbar-nav ml-auto\">
+                            <li class=\"nav-item\" role=\"presentation\"><a class=\"nav-link active\" href=\"index.html\" target=\"_top\">Bienvenue</a></li>
+                            <li class=\"nav-item\" role=\"presentation\"><a class=\"nav-link active\" href=\"cameraalouer.html\">Caméras à louer</a></li>
+                            <li class=\"nav-item\" role=\"presentation\"><a class=\"nav-link active\" href=\"lesloueurs.html\">Les loueurs</a></li>
+                            <li class=\"nav-item\" role=\"presentation\"><a class=\"nav-link active\" href=\"mettreunecameralocation.html\">Mettre une caméra en location </a></li>
+                            <li class=\"nav-item\" role=\"presentation\"><a class=\"nav-link active\" href=\"modifieruneannonce.html\">Modifier une annonce</a></li>
+                            <li class=\"nav-item\" role=\"presentation\"></li>
+                            <li class=\"nav-item\" role=\"presentation\"></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div class=\"container hero\">
+                <div class=\"row\">
+                    <div class=\"col-12 col-lg-6 col-xl-6 offset-xl-1\" style=\"margin-left:44px;\">
+                        <h1 style=\"margin-top:204px;\">La révolution de vos tournages</h1>
+                        <p>Vous allez pouvoir louer la caméra de votre choix à un particulier avec entre vous un contrat d'assurance.</p><button class=\"btn btn-light btn-lg action-button\" style=\"padding-right:20px;padding-left:20px;padding-bottom:6px;padding-top:6px;font-size:16px;\"
+                                                                                                                                                 hover=\"background: #fff; color: #333;\" type=\"button\">En savoir plus</button></div>
+                    <div class=\"col-md-5 col-lg-5 offset-lg-1 offset-xl-0 d-none d-lg-block phone-holder\"><img src=\"/img/VIPER_THOMSON.png\">
+                        <div class=\"iphone-mockup\">
+                            <div class=\"screen\"><img style=\"width:261px;\" src=\"assets/img/VIPER_THOMSON.png\"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class=\"footer-dark\">
+        <footer>
+            <div class=\"container\">
+                <div class=\"row\">
+                    <div class=\"col-sm-6 col-md-3 item\">
+                        <h3>Services</h3>
+                        <ul>
+                            <li><a href=\"#\">Collaborer avec nous</a></li>
+                            <li><a href=\"#\">Cookies</a></li>
+                            <li><a href=\"#\">Mentions légales</a></li>
+                            <li><a href=\"#\">Se connecter</a></li>
+                        </ul>
+                    </div>
+                    <div class=\"col-sm-6 col-md-3 item\">
+                        <h3>A propos</h3>
+                        <ul>
+                            <li><a href=\"#\">La société</a></li>
+                            <li><a href=\"#\">La team</a></li>
+                            <li><a href=\"#\">Nous rejoindre</a></li>
+                        </ul>
+                    </div>
+                    <div class=\"col-md-6 item text\">
+                        <h3>RENTACAM</h3>
+                        <p>Service de mise en relation de particuliers pour des fins de location de matériel cinématographique avec assurance.</p>
+                    </div>
+                    <div class=\"col item social\"><a href=\"#\"><i class=\"icon ion-social-facebook\"></i></a><a href=\"#\"><i class=\"icon ion-social-twitter\"></i></a><a href=\"#\"><i class=\"icon ion-social-instagram\"></i></a></div>
+                </div>
+                <p class=\"copyright\">RENTACAM © 2019</p>
+            </div>
+        </footer>
+    </div>
+{% endblock %}
+", "index/index.html.twig", "/Users/srinathchristophersamarasinghe/Documents/WEB DEVELOPMENT/RENTACAM/RENTACAM/templates/index/index.html.twig");
+    }
+}
